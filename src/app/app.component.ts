e489b67855990b5c30723a36bd403b7a24a77b3e
KakitapalliSunilKiran <k.sunilkiran98@gmail.com>
2021-05-01 11:59:04 +0530

    pushing to origin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Task1';
-}
+  name = 'Task144444444444';
+  team ="KKR";
+  email="abc@gmail.com"
+  runs=333;
+  strikerate =66;
+  flag=false;
+  ballsbowled = 44;
+  pSongs ="sriram"
+ 
+  play(){
+    alert("button clicked")
+  }
+  change(){
+    alert("change called..")
+  }
+  
+ // sendText(event : Event){
+  //  alert("entered text is : " + (<HTMLInputElement>event.target).value)
+  //  this.enteredValue = (<HTMLInputElement>event.target).value;
+   // this.enteredValue = event.target.value;
+   // this.propertyDemo = this.enteredValue + " captain ";
+    //alert(this.enteredValue)
+  }
+
+  
+
+
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HelloComponent } from './hello/hello.component';
+import { SunComponent } from './sun/sun.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloComponent,
+    SunComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, FormsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, FormsModule]
 })
 export class AppModule { }
